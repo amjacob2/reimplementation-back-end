da58f2903fceafdcade3f184b95ff801b476b265
Joe Johnson <jdjohns4@ncsu.edu>
2023-03-21 12:06:04 -0400

    added type filter to quiz question controller show method

--- app/controllers/api/v1/quiz_questions_controller.rb
+++ app/controllers/api/v1/quiz_questions_controller.rb
@@ -1,40 +1,64 @@
 class Api::V1::QuizQuestionsController < ApplicationController
     # GET /api/v1/quiz_questions
     def index
-      assignments = "testing"
+      # using this method to create new model objs in the database
+      # TODO: this is temporary
 
-      questionnaire = Questionnaire.new
-      #questionnaire.id = 1
-      questionnaire.name = 'Questions'
-      questionnaire.max_question_score = 10
-      questionnaire.save!
+      # questionnaire = Questionnaire.new
+      # #questionnaire.id = 1
+      # questionnaire.name = 'Questions'
+      # questionnaire.max_question_score = 10
+      # questionnaire.save!
 
-      quiz_question = TrueFalse.new
+      quiz_question = MultipleChoiceCheckbox.new
       #quiz_question.id = 99
-      quiz_question.type = 'TrueFalse'
-      quiz_question.txt = 'Is this a true/false question?'
+      quiz_question.type = 'MultipleChoiceCheckbox'
+      quiz_question.txt = 'Which of the following are NC State mascots?'
       quiz_question.weight = 5
-      quiz_question.questionnaire_id = questionnaire.id
+      quiz_question.questionnaire_id = 2
       quiz_question.seq = 1
-      quiz_question.save!
+      # quiz_question.save!
 
-      quiz_question_choice1 = QuizQuestionChoice.new
-      quiz_question_choice1.txt = 'True'
-      quiz_question_choice1.question_id = quiz_question.id
-      quiz_question_choice1.iscorrect = true
-      quiz_question_choice1.save!
+      # quiz_question_choice1 = QuizQuestionChoice.new
+      # quiz_question_choice1.txt = 'Mr. Wuf'
+      # quiz_question_choice1.question_id = quiz_question.id
+      # quiz_question_choice1.iscorrect = true
+      # quiz_question_choice1.save!
 
-      quiz_question_choice2 = QuizQuestionChoice.new
-      quiz_question_choice2.txt = 'False'
-      quiz_question_choice2.question_id = quiz_question.id
-      quiz_question_choice2.iscorrect = false
-      quiz_question_choice2.save!
+      # quiz_question_choice2 = QuizQuestionChoice.new
+      # quiz_question_choice2.txt = 'Tuffy'
+      # quiz_question_choice2.question_id = quiz_question.id
+      # quiz_question_choice2.iscorrect = true
+      # quiz_question_choice2.save!
 
-      render json: quiz_question.edit()
+      # quiz_question_choice3 = QuizQuestionChoice.new
+      # quiz_question_choice3.txt = 'Mrs. Wuf'
+      # quiz_question_choice3.question_id = quiz_question.id
+      # quiz_question_choice3.iscorrect = true
+      # quiz_question_choice3.save!
+
+      # quiz_question_choice4 = QuizQuestionChoice.new
+      # quiz_question_choice4.txt = 'Demon Deacon'
+      # quiz_question_choice4.question_id = quiz_question.id
+      # quiz_question_choice4.iscorrect = false
+      # quiz_question_choice4.save!
+
+      render json: quiz_question
     end
 
     def show
-      quiz_question = QuizQuestion.all
-      render json: quiz_question
+      type = params[:type]
+
+      quiz_question = []
+
+      if type == 'TrueFalse' then
+        quiz_question = TrueFalse.all
+      elsif type == 'MultipleChoiceRadio' then
+        quiz_question = MultipleChoiceRadio.all
+      elsif type == 'MultipleChoiceCheckbox' then
+        quiz_question = MultipleChoiceCheckbox.all
+      end
+      
+      render json: quiz_question.to_json(:include => :quiz_question_choices)
     end
 end
